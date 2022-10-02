8c2713e536cf15d81b9de8ca7337dc664822dbad
Abdur Rakib <abdurrakib9089@gmail.com>
2022-10-03 00:21:38 +0530

    second commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,12 +11,12 @@ function Footer(props) {
                         <h5>Sorting Visualizer</h5>
                     </div>
                     <div style={{fontStyle: "italic"}}>
-		              Salman Shaikh<br />
+		              Abdur Rakib<br />
 		              B.E. in Information Technology'23<br/>
                       Jadavpur University<br />
                       <p className="footer__text">
                         © 2022 - developed by
-                        <a rel="noreferrer" href="https://github.com/ssk-2002" target="_blank"> Salman Shaikh</a>
+                        <a rel="noreferrer" href="https://github.com/Rakib12345678/" target="_blank">Abdur Rakib</a>
                       </p>
                     </div>
                 </div>
@@ -24,9 +24,9 @@ function Footer(props) {
                 {/* this div is for contact info-> mail, linkedin, github */}
                 <div className="col-12 col-sm-3 col-md-2 mt-3 align-self-center">
                     <div>
-                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/salman-shaikh-82989b1b9/"><i className="fa fa-linkedin"></i></a>{' '}
-                        <br /> <br /> <a className="btn btn-social-icon" href="mailto:salmanhere101@gmail.com"><i className="fa fa-envelope-o"></i></a>
-                        <br /> <br /> <a className="btn btn-social-icon" rel="noreferrer" href="https://github.com/ssk-2002" target="_blank"> <i class="fa fa-github"></i></a>
+                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/abdur-rakib-094888193/"><i className="fa fa-linkedin"></i></a>{' '}
+                        <br /> <br /> <a className="btn btn-social-icon" href="mailto:abdurrakib9089@gmail.com"><i className="fa fa-envelope-o"></i></a>
+                        <br /> <br /> <a className="btn btn-social-icon" rel="noreferrer" href="https://github.com/Rakib12345678/" target="_blank"> <i class="fa fa-github"></i></a>
                     </div>
                 </div>
             </div>
